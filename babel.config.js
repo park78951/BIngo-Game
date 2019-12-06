a5603f8f669395199297d8526e5142b74f34fc22
Sangkyu <park78951@gmail.com>
2019-12-07 03:58:24 +0900

    docs: remove unnecessary dependencies

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = (api) => {
 
   const presets = ["@babel/preset-env", "@babel/preset-react"];
   const plugins = [
-    "@babel/plugin-proposal-class-properties",
     "babel-plugin-styled-components",
     "react-hot-loader/babel",
     "@babel/plugin-transform-runtime",
